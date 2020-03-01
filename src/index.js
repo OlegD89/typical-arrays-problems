eabc47897bd51d3ce72d188204ad2f37ecc27778
dubkooleg@gmail.com <dubkooleg@gmail.com>
2020-03-01 14:30:09 +0300

    feat: methods max,min,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) {  
+  var res = array && array.length ? Math.min.apply(null, array) : 0;
+  //console.log(res)
+  return res
 }
 
 exports.max = function max (array) {
-  return 0;
+  var res = array && array.length ? Math.max.apply(null, array) : 0;
+  //console.log(res)
+  return res
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || !array.length)
+    return 0;
+
+  var sum = 0;
+  for( var i = 0; i < array.length; i++ ){
+      sum += parseInt( array[i]);
+  }
+
+  return sum/array.length;
 }
